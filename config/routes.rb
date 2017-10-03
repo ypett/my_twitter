d4c8dcb351dacebd0e3ca95a6fc06985eafe3fd7
Yvette Pettinelli <ipettinelli@att.net>
2017-10-03 10:56:03 -0500

    Add Tweet scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :tweets
   devise_for :users
+
+  root 'tweets#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003144236) do
+ActiveRecord::Schema.define(version: 20171003153221) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

482a428c4d7b78613a7d6d9f51ba748ebd3106d2
Yvette Pettinelli <ipettinelli@att.net>
2017-10-04 11:19:32 -0500

    Add following views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  get 'feed' => 'epicenter#feed'
+
+  get 'show_user' => 'epicenter#show_user'
+
+  get 'now_following' => 'epicenter#now_following'
+
+  get 'unfollow' => 'epicenter#unfollow'
+
   resources :tweets
   devise_for :users
 
-  root 'tweets#index'
+  # root 'tweets#index'
+  root 'epicenter#feed'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004134129) do
+ActiveRecord::Schema.define(version: 20171004154131) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171004134129) do
     t.text "bio"
     t.string "location"
     t.string "avatar"
+    t.text "following"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
